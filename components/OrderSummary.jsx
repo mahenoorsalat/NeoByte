93868b30c84da74dfaaa49ba985efc1b478d3973
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2025-08-29 13:56:29 +0530

    added

--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -1,5 +1,6 @@
 import { addressDummyData } from "@/assets/assets";
 import { useAppContext } from "@/context/AppContext";
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 

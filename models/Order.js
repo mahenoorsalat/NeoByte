9aadf02973649c9e790497a9d6f472e3fbf7852f
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2025-09-02 10:09:45 +0530

    add

--- models/Order.js
+++ models/Order.js
@@ -1,25 +1,18 @@
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
-  userId: { type: String, required: true, ref: "user" },
-  items: [
-    {
-      product: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
-      quantity: { type: Number, required: true },
-    },
-  ],
-  address: {
-    fullName: { type: String, required: true },
-    phoneNumber: { type: String, required: true },
-    pincode: { type: String, required: true },
-    area: { type: String, required: true },
-    city: { type: String, required: true },
-    state: { type: String, required: true },
-  },
-  amount: { type: Number, required: true },
-  status: { type: String, default: "Order Placed" },
+  userId: { type: String, required: true , ref:'user' },
+Items: [{
+  product: { type: String, required: true, ref: 'product' },
+  quantity: { type: Number, required: true }
+}],
+phoneNumber: { type: Number, required: true },
+pincode: { type: String, ref: 'address', required: true },
+  status: { type: String, required: true , default:'Order Placed' },
   date: { type: Number, required: true },
+
 });
 
-const Order = mongoose.models.Order || mongoose.model("Order", orderSchema);
+const Order =
+  mongoose.models.order || mongoose.model("order", orderSchema);
 export default Order;

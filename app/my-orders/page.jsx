c83f30a43ff59f9f97a078d35bfcfb2a67d3e013
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2025-09-08 10:02:02 +0530

    add

--- app/my-orders/page.jsx
+++ app/my-orders/page.jsx
@@ -62,15 +62,16 @@ const MyOrders = () => {
                                     </p>
                                 </div>
                                 <div>
-                                    <p>
-                                        <span className="font-medium">{order.address.fullName}</span>
-                                        <br />
-                                        <span >{order.address.area}</span>
-                                        <br />
-                                        <span>{`${order.address.city}, ${order.address.state}`}</span>
-                                        <br />
-                                        <span>{order.address.phoneNumber}</span>
-                                    </p>
+                               <p>
+  <span className="font-medium">{order.address.fullName}</span>
+  <br />
+  <span>{order.address.area}</span>
+  <br />
+  <span>{`${order.address.city}, ${order.address.state}`}</span>
+  <br />
+  <span>{order.address.phoneNumber}</span>
+</p>
+
                                 </div>
                                 <p className="font-medium my-auto">{currency}{order.amount}</p>
                                 <div>

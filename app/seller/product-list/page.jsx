7f2328c5df87379a04243d253d64ae3f62639c47
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2025-09-08 12:24:29 +0530

    added

--- app/seller/product-list/page.jsx
+++ app/seller/product-list/page.jsx
@@ -18,10 +18,9 @@ const ProductList = () => {
     try {
       const { token } = await getToken();
 
-     const { data } = await axios.get("/api/product/list", {
-  headers: { Authorization: `Bearer ${token}` },
-});
-
+      const { data } = await axios.get("/api/product/seller-list", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
 
       if (data.success) {
         setProducts(Array.isArray(data.products) ? data.products : []); // ✅ always an array

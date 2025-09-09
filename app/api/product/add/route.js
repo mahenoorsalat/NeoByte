f01be1f196908896e3d87a56f7e023316eef7332
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2025-09-09 10:17:48 +0530

    add

--- app/api/product/add/route.js
+++ app/api/product/add/route.js
@@ -1,8 +1,7 @@
 import connectDB from "@/config/db";
 import authSeller from "@/lib/authSeller";
 import Product from "@/models/Product";
-
-import { auth } from "@clerk/nextjs/server";
+import { getAuth } from "@clerk/nextjs/server";
 import { v2 as cloudinary } from "cloudinary";
 import { NextResponse } from "next/server";
 

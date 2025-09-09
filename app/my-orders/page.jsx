7e3f608a44d4918235bc044ecab23596d13ab4aa
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2025-09-09 10:39:26 +0530

    add

--- app/my-orders/page.jsx
+++ app/my-orders/page.jsx
@@ -55,9 +55,12 @@ const MyOrders = () => {
                                         alt="box_icon"
                                     />
                                     <p className="flex flex-col gap-3">
-                                        <span className="font-medium text-base">
-                                            {order.items.map((item) => item.product.name + ` x ${item.quantity}`).join(", ")}
-                                        </span>
+                                      <span className="font-medium text-base">
+  {order.items.map((item) => 
+    `${item.product?.name || "Unknown Product"} x ${item.quantity}`
+  ).join(", ")}
+</span>
+
                                         <span>Items : {order.items.length}</span>
                                     </p>
                                 </div>
